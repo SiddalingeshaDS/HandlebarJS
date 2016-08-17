aa1bd3d8f9fffaf2b9b9a65b6d8324ea53e438d3
Siddalingesha D S <siddhudev813@gmail.com>
2016-08-17 20:29:42 +0530

    Adding bootstrap fonts to build

--- build/server/templates/styles.js
+++ build/server/templates/styles.js
@@ -1,3 +1,3 @@
 var Handlebars = require("handlebars/runtime");module.exports = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "<link rel=\"stylesheet\" href=\"/css/lib/bootstrap/bootstrap.min.css\">\r\n";
+    return "<link rel=\"stylesheet\" href=\"/css/lib/bootstrap/css/bootstrap.min.css\">\r\n";
 },"useData":true});
