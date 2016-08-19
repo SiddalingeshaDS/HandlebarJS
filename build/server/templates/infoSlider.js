45a79e58829170274a0616278517964a141c5b11
Siddalingesha D S <siddhudev813@gmail.com>
2016-08-19 14:49:52 +0530

    added styles to infoSlider

--- build/server/templates/infoSlider.js
+++ build/server/templates/infoSlider.js
@@ -1,7 +1,7 @@
 var Handlebars = require("handlebars/runtime");module.exports = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var stack1, helper;
 
-  return "<div class=\"container\">\r\n  "
+  return "<div class=\"container info-slider-wrapper\">\r\n <div class=\"info-slider-header\">\r\n   <h2>Dig Deeper</h2>\r\n </div>\r\n <div class=\"info-slider-tiles\">\r\n  "
     + ((stack1 = ((helper = (helper = helpers.tiles || (depth0 != null ? depth0.tiles : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"tiles","hash":{},"data":data}) : helper))) != null ? stack1 : "")
-    + "\r\n</div>";
+    + "\r\n </div>\r\n</div>";
 },"useData":true});
