92f85ab4392ed42bbea495d51a16656342b23150
Siddalingesha D S <siddhudev813@gmail.com>
2016-08-19 12:42:24 +0530

    Added styling changes

--- build/server/templates/featured.js
+++ build/server/templates/featured.js
@@ -1,7 +1,7 @@
 var Handlebars = require("handlebars/runtime");module.exports = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var stack1, helper;
 
-  return "<div class=\"container\">\r\n  "
+  return "<div class=\"container featured-list\">\r\n  "
     + ((stack1 = ((helper = (helper = helpers.tiles || (depth0 != null ? depth0.tiles : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"tiles","hash":{},"data":data}) : helper))) != null ? stack1 : "")
     + "\r\n</div>";
 },"useData":true});
